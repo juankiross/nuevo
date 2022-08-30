efec132a13756b2c73565c2445d5e0c569f0ae55
Juan Carlos <juancarlosrosado7@hotmail.com>
2022-08-30 19:27:10 +0200

    añadiendo espacio en rama-b

--- archivo1.py
+++ archivo1.py
@@ -1,7 +1,10 @@
 vocales=['a','e','i','o','u']
+espacio=[' ']
 palabra=str(input('escribe una palabra para deletrear '))
 for i in palabra:
     if i in vocales:
         print(i, 'es una vocal')
+    elif i in espacio:
+        print('es un espacio')
     else:
         print(i, 'es una consonante')
